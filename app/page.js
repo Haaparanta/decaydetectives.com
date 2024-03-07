6fa141c02b1d4526c77f88c7935d14781f5714eb
vesa haaparanta <vesahaaparanta@hotmail.fi>
2024-03-07 18:40:21 +0200

    Update package.json and package-lock.json

--- app/page.js
+++ app/page.js
@@ -1,123 +1,143 @@
-import Image from "next/image";
+"use client";
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+
 
 export default function Home() {
+  const containerVariants = {
+    hidden: { opacity: 0, scale: 0.95 },
+    visible: { 
+      opacity: 1, 
+      scale: 1,
+      transition: { delayChildren: 0.3, staggerChildren: 0.2 }
+    },
+  };
+
+  const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    visible: { y: 0, opacity: 1, transition: { duration: 0.6 } },
+  };
+
+  const sections = [
+    {
+      title: "Welcome to the forefront of sustainable forest management and conservation",
+      content: "Where cutting-edge technology meets environmental stewardship. \n Our approach leverages Lidar data, revolutionizing how we understand and protect our precious forest ecosystems. \n Lidar, which stands for Light Detection and Ranging, enables us to create detailed, three-dimensional maps of forested areas with unparalleled accuracy. \n By utilizing Lidar technology, we can precisely measure the height, structure, and density of vegetation, allowing for a comprehensive analysis of forest health. \n Our forest management and conservation strategies are rooted in this advanced Lidar data, empowering us to make informed decisions for sustainable practices. \n From identifying areas at risk of deforestation to monitoring the impact of climate change on tree health, Lidar data provides invaluable insights.",
+    },
+    {
+      title: "Key Benefits of Lidar in Forest Management and Conservation",
+      content: "Precision Mapping: Lidar enables us to create high-resolution, detailed maps, facilitating precise identification of forest features and characteristics. \n Biodiversity Monitoring: By analyzing Lidar data, we can assess biodiversity by mapping different vegetation layers and identifying habitat structures crucial for various species. \n Carbon Sequestration: Lidar aids in estimating carbon stocks by measuring biomass and tree density accurately, contributing to efforts aimed at mitigating climate change. Deforestation Detection: Timely detection of deforestation or illegal logging activities is possible through Lidar monitoring, enabling rapid response and intervention. Fire Risk Assessment: Lidar data assists in evaluating fire risk by assessing fuel loads and identifying areas susceptible to wildfires, facilitating proactive fire management. Ecosystem Health Monitoring: Lidar allows us to monitor changes in the forest ecosystem over time, helping to identify stressors, disease outbreaks, or invasive species. Our commitment to sustainable forest management is strengthened by the integration of Lidar technology. By harnessing the power of precise data, we pave the way for a future where forests thrive, biodiversity flourishes, and ecosystems remain resilient in the face of global challenges. Join us in embracing the next frontier of forest management and conservation, where innovation and environmental responsibility converge for a greener tomorrow.",
+    },
+    {
+      title: "Forest Management and Conservation",
+      content: "Improve forest management practices by accurately identifying and mapping dead wood. This information can help in implementing targeted conservation efforts and sustainable forestry practices. Develop algorithms that not only distinguish between healthy and dead wood but also provide insights into the reasons behind tree mortality, such as diseases, pests, or environmental factors. Develop a system that integrates LiDAR data with other remote sensing technologies, such as satellite imagery and drones, to create a comprehensive and real-time forest health monitoring solution. Create a user-friendly interface that translates LiDAR data into actionable insights, providing clear recommendations for forest management based on the identified dead wood patterns.",
+    },
+    {
+      title: "Integration with Carbon Offset Programs",
+      content: "Integrate the dead wood identification data with carbon offset programs, helping companies and governments quantify their contributions to carbon sequestration through forest conservation. Offer a service that verifies and certifies the impact of forest conservation efforts on carbon offset programs, providing a valuable tool for entities aiming to achieve carbon neutrality.",
+    },
+    {
+      title: "International Environmental Compliance Solutions",
+      content: "Develop a system that uses LiDAR data to ensure compliance with international environmental agreements related to forest conservation and biodiversity. Provide consulting services and software solutions to countries and organizations striving to meet their commitments under international environmental agreements.",
+    },
+    
+    {
+      title: "Global Forest Health Monitoring Platform",
+      content: "Innovation: Develop a cloud-based platform that uses LiDAR data to monitor forest health globally, allowing stakeholders from different countries to access real-time information. Business Idea: Offer a subscription-based service to governments, environmental agencies, and forestry companies worldwide, providing them with a comprehensive tool for managing and conserving their forests.",
+    },
+    {
+      title: "Cross-Border Collaboration for Forest Conservation",
+      content: "Innovation: Implement a collaborative framework that allows countries to share and analyze LiDAR data collectively, fostering international cooperation for forest conservation efforts. Business Idea: Act as a facilitator for cross-border partnerships, offering consulting services and technology solutions to countries interested in jointly addressing global forest health challenges.",
+    },
+    {
+      title: "AI-Driven Global Forest Health Prediction",
+      content: "Innovation: Use artificial intelligence to analyze historical LiDAR data, satellite imagery, and climate data to predict and model future global forest health trends. Business Idea: Provide predictive analytics as a service to international organizations, governments, and NGOs, helping them proactively address potential threats to global forests.",
+    },
+    {
+      title: "Customized LiDAR Solutions for Different Ecosystems",
+      content: "Innovation: Tailor the dead wood identification algorithms to suit various ecosystems around the world, considering different tree species and environmental conditions. Business Idea: Develop and sell specialized LiDAR hardware and software packages optimized for specific global regions, catering to the unique characteristics of each ecosystem.",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+
+  ];
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <h1 className="text-white text-6xl font-bold">Decay Detectives</h1>
-      <h4 className="text-white text-3xl font-bold">Identification and classification of dead words and healthy woods by using LiDAR Data set</h4>
-      
-      <Image
-        src="/logo.webp"
-        alt="Decay Detectives"
-        width={50}
-        height={50}
-      />
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">Forest Management and Conservation</h2>
-        <p className="text-white text-lg font-bold">
-          Improve forest management practices by accurately identifying and mapping dead wood. This information can help in implementing targeted conservation efforts and sustainable forestry practices.
-          Develop algorithms that not only distinguish between healthy and dead wood but also provide insights into the reasons behind tree mortality, such as diseases, pests, or environmental factors
-          Develop a system that integrates LiDAR data with other remote sensing technologies, such as satellite imagery and drones, to create a comprehensive and real-time forest health monitoring solution
-          Create a user-friendly interface that translates LiDAR data into actionable insights, providing clear recommendations for forest management based on the identified dead wood patterns.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">Integration with Carbon Offset Programs</h2>
-        <p className="text-white text-lg font-bold">
-          Integrate the dead wood identification data with carbon offset programs, helping companies and governments quantify their contributions to carbon sequestration through forest conservation.
-          Offer a service that verifies and certifies the impact of forest conservation efforts on carbon offset programs, providing a valuable tool for entities aiming to achieve carbon neutrality.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">International Environmental Compliance Solutions</h2>
-        <p className="text-white text-lg font-bold">
-          Develop a system that uses LiDAR data to ensure compliance with international environmental agreements related to forest conservation and biodiversity.
-          Provide consulting services and software solutions to countries and organizations striving to meet their commitments under international environmental agreements.
-
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">Innovation</h2>
-        <p className="text-white text-lg font-bold">
-          Develop a cloud-based platform that uses LiDAR data to monitor forest health globally, allowing stakeholders from different countries to access real-time information.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">Business Idea</h2>
-        <p className="text-white text-lg font-bold">
-          Offer a subscription-based service to governments, environmental agencies, and forestry companies worldwide, providing them with a comprehensive tool for managing and conserving their forests.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">Cross-Border Collaboration for Forest Conservation</h2>
-        <p className="text-white text-lg font-bold">
-          Innovation: Implement a collaborative framework that allows countries to share and analyze LiDAR data collectively, fostering international cooperation for forest conservation efforts.
-          Business Idea: Act as a facilitator for cross-border partnerships, offering consulting services and technology solutions to countries interested in jointly addressing global forest health challenges.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">AI-Driven Global Forest Health Prediction</h2>
-        <p className="text-white text-lg font-bold">
-          Innovation: Use artificial intelligence to analyze historical LiDAR data, satellite imagery, and climate data to predict and model future global forest health trends.
-          Business Idea: Provide predictive analytics as a service to international organizations, governments, and NGOs, helping them proactively address potential threats to global forests.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">Customized LiDAR Solutions for Different Ecosystems</h2>
-        <p className="text-white text-lg font-bold">
-          Innovation: Tailor the dead wood identification algorithms to suit various ecosystems around the world, considering different tree species and environmental conditions.
-          Business Idea: Develop and sell specialized LiDAR hardware and software packages optimized for specific global regions, catering to the unique characteristics of each ecosystem.
-        </p>
-      </div>
-      
-      <div>
-        <h2 className="text-white text-2xl font-bold">Business Model</h2>
-        <p className="text-white text-lg font-bold">
-          Empower forest managers and policymakers with data-driven insights to make informed decisions regarding forest conservation and resource allocation.
-          Foster collaboration with environmental agencies to implement the developed technology for large-scale monitoring and conservation efforts.
-          Create a platform that facilitates data sharing and collaboration among various stakeholders, including government bodies, researchers, and forestry companies.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold">Educational Initiatives</h2>
-        <p className="text-white text-lg font-bold">
-          Impact: Raise awareness about the importance of forest health and dead wood identification among the general public, schools, and environmental organizations.
-          Innovation: Develop educational programs, apps, or interactive tools that use LiDAR data to engage people in understanding the ecological significance of identifying and addressing dead wood in forests.
-          Innovation: Create an educational program that uses the project's findings to raise awareness about the importance of global forest health and the role of dead wood identification in biodiversity conservation.
-          Business Idea: Offer educational resources, workshops, and online courses to schools, universities, and environmental organizations worldwide, contributing to a global understanding of forest ecosystems.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold"></h2>
-        <p className="text-white text-lg font-bold">
-          
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold"></h2>
-        <p className="text-white text-lg font-bold">
-          
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-white text-2xl font-bold"></h2>
-        <p className="text-white text-lg font-bold">
-          
-        </p>
-      </div>
-
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        className="space-y-10 w-full text-center"
+      >
+        <motion.h1 variants={itemVariants} className="text-white text-6xl font-bold">
+          Decay Detectives
+        </motion.h1>
+        <motion.h4 variants={itemVariants} className="text-white text-3xl">
+          Identification and classification of dead words and healthy woods using LiDAR Dataset
+        </motion.h4>
+        <motion.div variants={itemVariants} className="my-5">
+          <Image
+            src="/logo.webp"
+            alt="Decay Detectives Logo"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        </motion.div>
+        {sections.map((section, index) => (
+          <motion.div key={index} variants={itemVariants}>
+            <h2 className="text-white text-2xl font-bold">{section.title}</h2>
+            <p className="text-white text-lg">{section.content}</p>
+          </motion.div>
+        ))}
+      </motion.div>
     </main>
   );
 }
